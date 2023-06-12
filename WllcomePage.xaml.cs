de1fbe9b319123cfc546f1d4cfa05ed60f778789
Elena <chailikeg@yahoo.com>
2023-06-12 14:39:40 -0400

    WellcomePgeGUI

--- WllcomePage.xaml.cs
+++ WllcomePage.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Lab1_DesktopAppDev
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WllcomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WllcomePage : Window
     {
-        public MainWindow()
+        public WllcomePage()
         {
             InitializeComponent();
         }
-
     }
 }
